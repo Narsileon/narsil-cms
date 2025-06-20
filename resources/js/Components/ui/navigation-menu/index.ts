792802b842fed804c7ba050789af7c1035c00919
Narsilien <rigaux.jonathan@gmail.com>
2025-06-20 06:30:06 +0200

    Add navigation menu

---
+++ resources/js/Components/ui/navigation-menu/index.ts
@@ -0,0 +1,38 @@
+import NavigationMenu from "./navigation-menu";
+import NavigationMenuContent from "./navigation-menu-content";
+import NavigationMenuIndicator from "./navigation-menu-indicator";
+import NavigationMenuItem from "./navigation-menu-item";
+import NavigationMenuLink from "./navigation-menu-link";
+import NavigationMenuList from "./navigation-menu-list";
+import NavigationMenuTrigger from "./navigation-menu-trigger";
+import NavigationMenuViewport from "./navigation-menu-viewport";
+import type { NavigationMenuContentProps } from "./navigation-menu-content";
+import type { NavigationMenuIndicatorProps } from "./navigation-menu-indicator";
+import type { NavigationMenuItemProps } from "./navigation-menu-item";
+import type { NavigationMenuLinkProps } from "./navigation-menu-link";
+import type { NavigationMenuListProps } from "./navigation-menu-list";
+import type { NavigationMenuProps } from "./navigation-menu";
+import type { NavigationMenuTriggerProps } from "./navigation-menu-trigger";
+import type { NavigationMenuViewportProps } from "./navigation-menu-viewport";
+
+export {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+};
+
+export type {
+  NavigationMenuContentProps,
+  NavigationMenuIndicatorProps,
+  NavigationMenuItemProps,
+  NavigationMenuLinkProps,
+  NavigationMenuListProps,
+  NavigationMenuProps,
+  NavigationMenuTriggerProps,
+  NavigationMenuViewportProps,
+};
