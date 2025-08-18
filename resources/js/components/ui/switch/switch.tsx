1e767a42c871329dd560c78ee74ab561861485a8
Narsilien <rigaux.jonathan@gmail.com>
2025-08-18 17:19:24 +0200

    Improve switch

--- resources/js/components/ui/switch/switch.tsx
+++ resources/js/components/ui/switch/switch.tsx
@@ -9,10 +9,8 @@ function Switch({ className, ...props }: SwitchProps) {
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none",
-        "dark:data-[state=unchecked]:bg-input/80",
-        "data-[state=checked]:bg-primary",
-        "data-[state=unchecked]:bg-input",
+        "peer inline-flex h-[1.15rem] w-8 shrink-0 cursor-pointer items-center rounded-full border border-transparent shadow-xs transition-all outline-none",
+        "data-[state=checked]:bg-constructive data-[state=unchecked]:bg-input",
         "disabled:cursor-not-allowed disabled:opacity-50",
         "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
         className,
@@ -22,8 +20,7 @@ function Switch({ className, ...props }: SwitchProps) {
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
         className={cn(
-          "bg-background pointer-events-none block size-4 rounded-full ring-0 transition-transform",
-          "dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground",
+          "bg-constructive-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform",
           "data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0",
         )}
       />
