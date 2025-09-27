cc16b7f10a77b08ccb6d9037cd3cb88058d5f528
Narsilien <rigaux.jonathan@gmail.com>
2025-09-27 16:00:01 +0200

    Fix item size

--- resources/js/blocks/data-table/data-table-columns.tsx
+++ resources/js/blocks/data-table/data-table-columns.tsx
@@ -166,7 +166,7 @@ function SortableItem({
   return (
     <div
       style={style}
-      className="flex items-center gap-2 overflow-hidden rounded-md border bg-background pr-1"
+      className="flex h-9 items-center gap-2 overflow-hidden rounded-md border bg-background pr-1"
     >
       <SortableHandle
         ref={setNodeRef}
