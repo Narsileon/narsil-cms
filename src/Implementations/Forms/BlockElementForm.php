df97771b64957f7cde2d3d990ce56d36846b33a9
Jonathan Rigaux <jonathan.rigaux@rheinschafe.de>
2025-12-17 14:17:47 +0100

    Improve haselement forms

--- src/Implementations/Forms/BlockElementForm.php
+++ src/Implementations/Forms/BlockElementForm.php
@@ -4,9 +4,11 @@ namespace Narsil\Implementations\Forms;
 
 #region USE
 
+use Narsil\Contracts\Fields\SwitchField;
 use Narsil\Contracts\Fields\TextField;
 use Narsil\Contracts\Forms\BlockForm as Contract;
 use Narsil\Implementations\AbstractForm;
+use Narsil\Models\Elements\Block;
 use Narsil\Models\Elements\BlockElement;
 use Narsil\Models\Elements\Field;
 
@@ -26,20 +28,46 @@ class BlockElementForm extends AbstractForm implements Contract
     protected function getLayout(): array
     {
         return [
-            new Field([
-                Field::HANDLE => BlockElement::NAME,
-                Field::NAME => trans('narsil::validation.attributes.name'),
-                Field::REQUIRED => true,
-                Field::TRANSLATABLE => true,
-                Field::TYPE => TextField::class,
-                Field::SETTINGS => app(TextField::class),
-            ]),
-            new Field([
-                Field::HANDLE => BlockElement::HANDLE,
-                Field::NAME => trans('narsil::validation.attributes.handle'),
-                Field::REQUIRED => true,
-                Field::TYPE => TextField::class,
-                Field::SETTINGS => app(TextField::class),
+            new Block([
+                Block::RELATION_ELEMENTS => [
+                    new BlockElement([
+                        BlockElement::RELATION_ELEMENT => new Field([
+                            Field::HANDLE => BlockElement::NAME,
+                            Field::NAME => trans('narsil::validation.attributes.name'),
+                            Field::REQUIRED => true,
+                            Field::TRANSLATABLE => true,
+                            Field::TYPE => TextField::class,
+                            Field::SETTINGS => app(TextField::class),
+                        ]),
+                    ]),
+                    new BlockElement([
+                        BlockElement::RELATION_ELEMENT => new Field([
+                            Field::HANDLE => BlockElement::HANDLE,
+                            Field::NAME => trans('narsil::validation.attributes.handle'),
+                            Field::REQUIRED => true,
+                            Field::TYPE => TextField::class,
+                            Field::SETTINGS => app(TextField::class),
+                        ]),
+                    ]),
+                    new BlockElement([
+                        BlockElement::WIDTH => 50,
+                        BlockElement::RELATION_ELEMENT =>  new Field([
+                            Field::HANDLE => BlockElement::REQUIRED,
+                            Field::NAME => trans('narsil::validation.attributes.required'),
+                            Field::TYPE => SwitchField::class,
+                            Field::SETTINGS => app(SwitchField::class),
+                        ]),
+                    ]),
+                    new BlockElement([
+                        BlockElement::WIDTH => 50,
+                        BlockElement::RELATION_ELEMENT => new Field([
+                            Field::HANDLE => BlockElement::TRANSLATABLE,
+                            Field::NAME => trans('narsil::validation.attributes.translatable'),
+                            Field::TYPE => SwitchField::class,
+                            Field::SETTINGS => app(SwitchField::class),
+                        ]),
+                    ]),
+                ],
             ]),
         ];
     }
--- src/Implementations/Forms/TemplateSectionForm.php
+++ src/Implementations/Forms/TemplateSectionForm.php
@@ -4,11 +4,15 @@ namespace Narsil\Implementations\Forms;
 
 #region USE
 
+use Narsil\Contracts\Fields\SwitchField;
 use Narsil\Contracts\Fields\TextField;
 use Narsil\Contracts\Forms\TemplateForm as Contract;
 use Narsil\Implementations\AbstractForm;
+use Narsil\Models\Elements\Block;
+use Narsil\Models\Elements\BlockElement;
 use Narsil\Models\Elements\Field;
 use Narsil\Models\Elements\TemplateSection;
+use Narsil\Models\Elements\TemplateSectionElement;
 
 #endregion
 
@@ -26,21 +30,50 @@ class TemplateSectionForm extends AbstractForm implements Contract
     protected function getLayout(): array
     {
         return [
-            new Field([
-                Field::HANDLE => TemplateSection::NAME,
-                Field::NAME => trans('narsil::validation.attributes.name'),
-                Field::REQUIRED => true,
-                Field::TRANSLATABLE => true,
-                Field::TYPE => TextField::class,
-                Field::SETTINGS => app(TextField::class),
+            new Block([
+                Block::RELATION_ELEMENTS => [
+                    new BlockElement([
+                        BlockElement::RELATION_ELEMENT => new Field([
+                            Field::HANDLE => TemplateSection::NAME,
+                            Field::NAME => trans('narsil::validation.attributes.name'),
+                            Field::REQUIRED => true,
+                            Field::TRANSLATABLE => true,
+                            Field::TYPE => TextField::class,
+                            Field::SETTINGS => app(TextField::class),
+                        ]),
+                    ]),
+                    new BlockElement([
+                        BlockElement::RELATION_ELEMENT => new Field([
+                            Field::HANDLE => TemplateSection::HANDLE,
+                            Field::NAME => trans('narsil::validation.attributes.handle'),
+                            Field::REQUIRED => true,
+                            Field::TYPE => TextField::class,
+                            Field::SETTINGS => app(TextField::class),
+                        ]),
+                    ]),
+                    new BlockElement([
+                        BlockElement::WIDTH => 50,
+                        BlockElement::RELATION_ELEMENT =>  new Field([
+                            Field::HANDLE => TemplateSectionElement::REQUIRED,
+                            Field::NAME => trans('narsil::validation.attributes.required'),
+                            Field::TYPE => SwitchField::class,
+                            Field::SETTINGS => app(SwitchField::class),
+                        ]),
+                    ]),
+                    new BlockElement([
+                        BlockElement::WIDTH => 50,
+                        BlockElement::RELATION_ELEMENT => new Field([
+                            Field::HANDLE => TemplateSectionElement::TRANSLATABLE,
+                            Field::NAME => trans('narsil::validation.attributes.translatable'),
+                            Field::TYPE => SwitchField::class,
+                            Field::SETTINGS => app(SwitchField::class),
+                        ]),
+                    ]),
+                ],
             ]),
-            new Field([
-                Field::HANDLE => TemplateSection::HANDLE,
-                Field::NAME => trans('narsil::validation.attributes.handle'),
-                Field::REQUIRED => true,
-                Field::TYPE => TextField::class,
-                Field::SETTINGS => app(TextField::class),
-            ]),
+
+
+
         ];
     }
 
