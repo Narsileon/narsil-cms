55ee6f0b5a2d4fb852d5f7f137d922b53da24cb9
Narsileon <rigaux.jonathan@gmail.com>
2025-12-23 12:48:03 +0100

    Prepare form resources

---
+++ src/Implementations/Resources/FormInputOptionResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Narsil\Implementations\Resources;
+
+#region USE
+
+use Illuminate\Http\Request;
+use Narsil\Contracts\Resources\FooterSocialLinkResource as Contract;
+use Narsil\Implementations\AbstractResource;
+
+#endregion
+
+/**
+ * @version 1.0.0
+ * @author Jonathan Rigaux
+ */
+class FormInputOptionResource extends AbstractResource implements Contract
+{
+    #region PUBLIC METHODS
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            //
+        ];
+    }
+
+    #endregion
+}
