336703c2499ac80e97ec276302aeb8fe889b831d
Narsilien <rigaux.jonathan@gmail.com>
2025-12-26 16:12:16 +0100

    Clean up

--- config/narsil/models/observers.php
+++ config/narsil/models/observers.php
@@ -11,12 +11,12 @@ return [
     |
     */
 
-    \Narsil\Models\Structures\Template::class => \Narsil\Observers\TemplateObserver::class,
     \Narsil\Models\Entities\Entity::class => \Narsil\Observers\EntityObserver::class,
     \Narsil\Models\Entities\EntityBlockField::class => \Narsil\Observers\EntityBlockFieldObserver::class,
     \Narsil\Models\Hosts\Host::class => \Narsil\Observers\HostObserver::class,
     \Narsil\Models\Hosts\HostLocale::class => \Narsil\Observers\HostLocaleObserver::class,
     \Narsil\Models\Sites\Site::class => \Narsil\Observers\SiteObserver::class,
     \Narsil\Models\Sites\SitePage::class => \Narsil\Observers\SitePageObserver::class,
+    \Narsil\Models\Structures\Template::class => \Narsil\Observers\TemplateObserver::class,
     \Narsil\Models\User::class => \Narsil\Observers\UserObserver::class,
 ];
