c5a6390c4ebc8db2761367602be816c894a0e67c
Narsilien <rigaux.jonathan@gmail.com>
2026-01-08 12:24:53 +0100

    Clean up old resources

--- config/narsil/bindings/resources.php
+++ config/narsil/bindings/resources.php
@@ -12,9 +12,5 @@ return [
     */
 
     \Narsil\Contracts\Resources\EntityResource::class => \Narsil\Implementations\Resources\EntityResource::class,
-    \Narsil\Contracts\Resources\FieldsetResource::class => \Narsil\Implementations\Resources\FieldsetResource::class,
-    \Narsil\Contracts\Resources\FormResource::class => \Narsil\Implementations\Resources\FormResource::class,
-    \Narsil\Contracts\Resources\InputOptionResource::class => \Narsil\Implementations\Resources\InputOptionResource::class,
-    \Narsil\Contracts\Resources\InputResource::class => \Narsil\Implementations\Resources\InputResource::class,
     \Narsil\Contracts\Resources\UserResource::class => \Narsil\Implementations\Resources\UserResource::class,
 ];
