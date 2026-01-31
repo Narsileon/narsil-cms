f490e1c7b66038eaff07911ff14de976fcd51dfa
Narsilien <rigaux.jonathan@gmail.com>
2026-01-31 13:33:51 +0100

    Fix context menu

--- resources/js/components/context-menu/index.ts
+++ resources/js/components/context-menu/index.ts
@@ -2,33 +2,37 @@
 // https://base-ui.com/react/components/context-menu
 
 import ContextMenuCheckboxItem from "./context-menu-checkbox-item";
-import ContextMenuContent from "./context-menu-content";
+import ContextMenuCheckboxItemIndicator from "./context-menu-checkbox-item-indicator";
 import ContextMenuGroup from "./context-menu-group";
 import ContextMenuItem from "./context-menu-item";
 import ContextMenuLabel from "./context-menu-label";
+import ContextMenuContent from "./context-menu-popup";
 import ContextMenuPortal from "./context-menu-portal";
+import ContextMenuPositioner from "./context-menu-positioner";
 import ContextMenuRadioGroup from "./context-menu-radio-group";
 import ContextMenuRadioItem from "./context-menu-radio-item";
+import ContextMenuRadioItemIndicator from "./context-menu-radio-item-indicator";
 import ContextMenuRoot from "./context-menu-root";
 import ContextMenuSeparator from "./context-menu-separator";
-import ContextMenuSub from "./context-menu-sub";
-import ContextMenuSubContent from "./context-menu-sub-content";
-import ContextMenuSubTrigger from "./context-menu-sub-trigger";
+import ContextMenuSubmenuRoot from "./context-menu-submenu-root";
+import ContextMenuSubmenuTrigger from "./context-menu-submenu-trigger";
 import ContextMenuTrigger from "./context-menu-trigger";
 
 export {
   ContextMenuCheckboxItem,
+  ContextMenuCheckboxItemIndicator,
   ContextMenuContent,
   ContextMenuGroup,
   ContextMenuItem,
   ContextMenuLabel,
   ContextMenuPortal,
+  ContextMenuPositioner,
   ContextMenuRadioGroup,
   ContextMenuRadioItem,
+  ContextMenuRadioItemIndicator,
   ContextMenuRoot,
   ContextMenuSeparator,
-  ContextMenuSub,
-  ContextMenuSubContent,
-  ContextMenuSubTrigger,
+  ContextMenuSubmenuRoot,
+  ContextMenuSubmenuTrigger,
   ContextMenuTrigger,
 };
