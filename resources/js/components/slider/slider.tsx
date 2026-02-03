6e8242c98e96a7760cc9f7bcd484d4b5bc7c1c24
Narsilien <rigaux.jonathan@gmail.com>
2026-02-03 18:18:22 +0100

    Move scroll area and slider

--- resources/js/components/slider/slider.tsx
+++ resources/js/components/slider/slider.tsx
@@ -7,9 +7,13 @@ import {
 } from "@narsil-cms/components/slider";
 import { useMemo, type ComponentProps } from "react";
 
-type SliderProps = ComponentProps<typeof SliderRoot>;
-
-function Slider({ defaultValue, value, min = 0, max = 100, ...props }: SliderProps) {
+function Slider({
+  defaultValue,
+  value,
+  min = 0,
+  max = 100,
+  ...props
+}: ComponentProps<typeof SliderRoot>) {
   const computedValue = useMemo(() => {
     return Array.isArray(value) ? value : Array.isArray(defaultValue) ? defaultValue : [min, max];
   }, [value, defaultValue, min, max]);
