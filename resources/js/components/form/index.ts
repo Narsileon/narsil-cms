613ff02e95f647ad437a52007aef4b3150bc235b
Narsilien <rigaux.jonathan@gmail.com>
2026-02-07 13:39:49 +0100

    Remove form description

--- resources/js/components/form/index.ts
+++ resources/js/components/form/index.ts
@@ -1,6 +1,5 @@
 import useForm from "./form-context";
 import FormCountry from "./form-country";
-import FormDescription from "./form-description";
 import FormElement from "./form-element";
 import FormField from "./form-field";
 import useFormField from "./form-field-context";
@@ -15,7 +14,6 @@ import FormTimestamp from "./form-timestamps";
 
 export {
   FormCountry,
-  FormDescription,
   FormElement,
   FormField,
   FormFieldLanguage,
