7cab5d860d63749818a83d41b5abd7fc21716212
Narsilien <rigaux.jonathan@gmail.com>
2026-02-28 12:26:15 +0100

    Remove ring color

--- resources/js/components/sidebar/sidebar-group-label.tsx
+++ resources/js/components/sidebar/sidebar-group-label.tsx
@@ -8,8 +8,7 @@ function SidebarGroupLabel({ className, render, ...props }: useRender.ComponentP
     props: mergeProps<"div">(
       {
         className: cn(
-          "flex h-8 shrink-0 items-center rounded-md bg-linear-to-r from-transparent to-transparent px-2 text-xs font-medium text-muted-foreground ring-ring outline-hidden transition-[margin,opacity] duration-300 ease-linear",
-          "focus-visible:ring-2",
+          "flex h-8 shrink-0 items-center rounded-md bg-linear-to-r from-transparent to-transparent px-2 text-xs font-medium text-muted-foreground outline-hidden transition-[margin,opacity] duration-300 ease-linear",
           "[&>svg]:size-4 [&>svg]:shrink-0",
           "group-data-[collapsible=icon]:-z-10 group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0",
           className,
