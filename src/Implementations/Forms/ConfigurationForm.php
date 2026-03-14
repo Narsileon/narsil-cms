fe6fa33274d2163f677c21953da89eaed6dcab09
Narsilien <rigaux.jonathan@gmail.com>
2026-03-14 09:51:23 +0100

    Use locale service

--- src/Implementations/Forms/ConfigurationForm.php
+++ src/Implementations/Forms/ConfigurationForm.php
@@ -5,14 +5,12 @@ namespace Narsil\Cms\Implementations\Forms;
 #region USE
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
-use Locale;
 use Narsil\Base\Http\Data\Forms\FieldData;
 use Narsil\Base\Http\Data\Forms\FormStepData;
 use Narsil\Base\Http\Data\Forms\Inputs\SelectInputData;
-use Narsil\Base\Http\Data\OptionData;
 use Narsil\Base\Implementations\Form;
+use Narsil\Base\Services\LocaleService;
 use Narsil\Base\Services\RouteService;
 use Narsil\Cms\Contracts\Forms\ConfigurationForm as Contract;
 use Narsil\Cms\Models\Configuration;
@@ -58,7 +56,7 @@ class ConfigurationForm extends Form implements Contract
                         id: Configuration::DEFAULT_LANGUAGE,
                         required: true,
                         input: new SelectInputData(
-                            options: $this->getLanguageOptions($frontendLanguages),
+                            options: LocaleService::languageOptions($frontendLanguages),
                         ),
                     ),
                 ],
@@ -71,42 +69,11 @@ class ConfigurationForm extends Form implements Contract
                         id: Configuration::DEFAULT_LANGUAGE,
                         required: true,
                         input: new SelectInputData(
-                            options: $this->getLanguageOptions($backendLanguages),
+                            options: LocaleService::languageOptions($backendLanguages),
                         ),
                     ),
                 ],
             ),
         ];
     }
-
-    /**
-     * Get the language options.
-     *
-     * @param string[] $languages
-     *
-     * @return OptionData[]
-     */
-    protected function getLanguageOptions(array $languages): array
-    {
-        $options = [];
-
-        foreach ($languages as $language)
-        {
-            $label = Locale::getDisplayLanguage($language, App::getLocale());
-
-            $options[] = new OptionData(
-                label: ucfirst($label),
-                value: $language
-            );
-        }
-
-        usort($options, function (OptionData $a, OptionData $b)
-        {
-            return strcasecmp($a->label, $b->label);
-        });
-
-        return array_values($options);
-    }
-
-    #endregion
 }
